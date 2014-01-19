3801877ca670f37def526742892cd84223650450
Sugisawa Koji <sugisawa@kca.biglobe.ne.jp>
2014-01-20 03:37:46 +0900

    package-info added

---
+++ src/main/java/com/orangesignal/jlha/package-info.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2014 OrangeSignal.com All rights reserved.
+ *
+ * これは Apache ライセンス Version 2.0 (以下、このライセンスと記述) に
+ * 従っています。このライセンスに準拠する場合以外、このファイルを使用
+ * してはなりません。このライセンスのコピーは以下から入手できます。
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ * 適用可能な法律がある、あるいは文書によって明記されている場合を除き、
+ * このライセンスの下で配布されているソフトウェアは、明示的であるか暗黙の
+ * うちであるかを問わず、「保証やあらゆる種類の条件を含んでおらず」、
+ * 「あるがまま」の状態で提供されるものとします。
+ * このライセンスが適用される特定の許諾と制限については、このライセンス
+ * を参照してください。
+ */
+
+/**
+ * OrangeSignal CSV 向けの "LHA Library for Java" を提供します。<p>
+ * "LHA Library for Java" の詳細及びライセンスについては、
+ * <a href="http://homepage1.nifty.com/dangan/Content/Program/Java/jLHA/jLHA.html">LHA Library for Java</a> をご覧下さい。
+ * 
+ * @author 杉澤 浩二
+ * @since 2.0.0
+ */
+package com.orangesignal.jlha;
