cf56bd3f1f927d65560b395a26ddb36b7ab03545
orangesignal <sugisawa@kca.biglobe.ne.jp>
2014-01-23 09:18:22 +0900

    #13 例外クラス追加

---
+++ src/main/java/com/orangesignal/csv/CsvTokenException.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.orangesignal.csv;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 区切り文字形式データトークンに関しての例外が発生した場合にスローされる例外を提供します。
+ * 
+ * @author Koji Sugisawa
+ * @since 2.1
+ */
+public class CsvTokenException extends IOException {
+
+	private static final long serialVersionUID = 3908133388773744080L;
+
+	/**
+	 * 区切り文字形式データトークンのリストを保持します。
+	 */
+	private List<CsvToken> tokens;
+
+	/**
+	 * 指定された詳細メッセージと区切り文字形式データトークンのリストを持つ {@link CsvTokenException} を構築します。
+	 * 
+	 * @param message　詳細メッセージ
+	 * @param tokens 区切り文字形式データトークンの値リスト
+	 */
+	public CsvTokenException(final String message, final List<CsvToken> tokens) {
+		super(message);
+		this.tokens = Collections.unmodifiableList(tokens);
+	}
+
+	/**
+	 * 区切り文字形式データトークンのリストを返します。
+	 * 
+	 * @return 区切り文字形式データトークンのリスト
+	 */
+	public List<CsvToken> getTokens() {
+		return tokens;
+	}
+
+}
---
+++ src/main/java/com/orangesignal/csv/CsvValueException.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.orangesignal.csv;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 区切り文字形式データトークンの値に関しての例外が発生した場合にスローされる例外を提供します。
+ * 
+ * @author Koji Sugisawa
+ * @since 2.1
+ */
+public class CsvValueException extends IOException {
+
+	private static final long serialVersionUID = -2047098167770188272L;
+
+	/**
+	 * 区切り文字形式データトークンの値リストを保持します。
+	 */
+	private List<String> values;
+
+	/**
+	 * 指定された詳細メッセージと区切り文字形式データトークンの値リストを持つ {@link CsvValueException} を構築します。
+	 * 
+	 * @param message　詳細メッセージ
+	 * @param values 区切り文字形式データトークンの値リスト
+	 */
+	public CsvValueException(final String message, final List<String> values) {
+		super(message);
+		this.values = Collections.unmodifiableList(values);
+	}
+
+	/**
+	 * 区切り文字形式データトークンの値リストを返します。
+	 * 
+	 * @return 区切り文字形式データトークンの値リスト
+	 */
+	public List<String> getValues() {
+		return values;
+	}
+
+}
